a62cb45cfa4b6e0cc50ec86cc995bf4c9f4f3e0f
vagrant <vagrant@lighthouse-labs>
2015-07-08 00:35:46 +0000

    after orm

--- contact_database.rb
+++ contact_database.rb
@@ -1,16 +1,33 @@
 ## TODO: Implement CSV reading/writing
 require 'csv'
 
-def get_raw_data
-  output = []
-  CSV.foreach('touch_contacts.csv') do |row|
-    output << row
-  end
-  output
-end
+class ContactDatabase
 
-def write_new(name, email, phone_numbers)
-      CSV.open('touch_contacts.csv', "a+") do |csv|
-        csv << ["name", "email", "phone_numbers"]
-      end
+  def self.connect
+    begin
+      conn = PG.connect( 
+        dbname: 'contactlist',
+        user: 'development',
+        host: 'localhost',
+        password: 'development'
+        )
+    rescue PG::PoorConnection
+      puts "Something is wrong"
     end
+  end
+
+  def get_raw_data
+    output = []
+    CSV.foreach('touch_contacts.csv') do |row|
+    output << row
+    end
+    output
+  end
+
+  def write_new(name, email, phone_numbers)
+    CSV.open('touch_contacts.csv', "a+") do |csv|
+    csv << ["name", "email", "phone_numbers"]
+    end
+  end
+
+end
